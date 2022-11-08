7025e5110d3988952c8eea0c3132f92998769798
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-08 16:09:06 +0100

    sconto 40%

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 const mybtn= document.getElementById("mybtn");
+const age=document.getElementById("age").value;
 const numeroKm=document.getElementById("numeroKm").value;
 const prezzo=2;
 const prezzointero=(numeroKm * prezzo).toFixed(2);
-var age;
-var sconto;
-var costo;
+
 
 mybtn.addEventListener("click",
 function(){
     const age=document.getElementById("age").value;
+    const numeroKm=document.getElementById("numeroKm").value;
+
 
     if (age < 17) {
         let sconto= ((numeroKm * prezzo) * 20) / 100 ;
@@ -18,7 +19,7 @@ function(){
     else if ( age >= 65){
         let sconto= (numeroKm * prezzo) * 40 / 100;
         let costo=(prezzointero - sconto).toFixed(2);
-        console.log(`La tua tariffa scontata del 20% è di ${costo}euro`);
+        console.log(`La tua tariffa scontata del 40% è di ${costo}euro`);
     }
     else{
         console.log(`La tua tariffa senza sconti è di ${prezzointero}euro`);
