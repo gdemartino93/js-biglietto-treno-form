1931a0378928cd67c4717ad87a5a5a9adbdb36b5
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-08 15:57:29 +0100

    sconti

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
-// prendi dati inseriti dall'user nell'input 
-let age=document.getElementById("age").value;
-let numeroKm=document.getElementById("numeroKm").value;
-
-function mybtn() {
-    console.log(`La tua età è ${age}`)
-    console.log(`Vuoi percorrere ${numeroKm} km`)
-}
-// calcola prezzo intero
-const prezzo=0.21;
+const mybtn= document.getElementById("mybtn");
+const numeroKm=document.getElementById("numeroKm").value;
+const prezzo=2;
 const prezzointero=(numeroKm * prezzo).toFixed(2);
+var age;
+var sconto;
+var costo;
+
+mybtn.addEventListener("click",
+function(){
+    const age=document.getElementById("age").value;
+
+    if (age < 17) {
+        let sconto= ((numeroKm * prezzo) * 20) / 100 ;
+        let costo= (prezzointero - sconto).toFixed(2);
+        console.log(`La tua tariffa scontata del 20% è di ${costo}euro`);
+    }
+    else if ( age >= 65){
+        let sconto= (numeroKm * prezzo) * 40 / 100;
+        let costo=(prezzointero - sconto).toFixed(2);
+        console.log(`La tua tariffa scontata del 20% è di ${costo}euro`);
+    }
+    else{
+        console.log(`La tua tariffa senza sconti è di ${prezzointero}euro`);
+    }
+   
+}
+);
 
