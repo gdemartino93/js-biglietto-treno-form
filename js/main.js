d988b83ee31e22f6fa58f17178eedbff9e7b1fd8
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-08 21:49:14 +0100

    full price line trough and price with discount

--- js/main.js
+++ js/main.js
@@ -13,11 +13,11 @@ function seisicuro() {
         document.getElementById("carrozzaOut").innerHTML="";
         document.getElementById("codiceOut").innerHTML="";
         document.getElementById("costoOut").innerHTML="";
+        document.getElementById("prezzointero").innerHTML="";
     } else {
       alert("Azione annullata");
     }
   })
-
 mybtn.addEventListener("click",
 function(){
     const userName=document.getElementById("userName").value;
@@ -35,10 +35,18 @@ function(){
         document.getElementById("divOff").style.display = "flex";
         document.getElementById("userNameOut").innerHTML=`${userName}`;
         document.getElementById("offertaOut").innerHTML=`${age}`;
-        document.getElementById("carrozzaOut").innerHTML=Math.floor(Math.random() * 11);
-        document.getElementById("codiceOut").innerHTML=Math.floor(Math.random() * 1000000);
+
+        // valore da 1 a 10, ESCLUSO ZERO
+
+        document.getElementById("carrozzaOut").
+        innerHTML=Math.floor(Math.random() * 10 ) + 1;
+
+        // Escluso ZERO
+
+        document.getElementById("codiceOut").innerHTML=Math.floor(Math.random() * 1000000) + 1;
+        document.getElementById("prezzointero").innerHTML=`${prezzointero}`
         document.getElementById("costoOut").innerHTML=`${costo}`;
-        console.log(`La tua tariffa scontata del 20% è di ${costo}euro`);
+      
     }
     else if ( age == "over65"){
         let sconto= (numeroKm * prezzo) * 40 / 100;
@@ -46,20 +54,33 @@ function(){
         document.getElementById("divOff").style.display = "flex";
         document.getElementById("userNameOut").innerHTML=`${userName}`;
         document.getElementById("offertaOut").innerHTML=`${age}`;
-        document.getElementById("carrozzaOut").innerHTML=Math.floor(Math.random() * 11);
+
+        // VALORE DA 1 A 10 ESCLUSO ZERO
+
+        document.getElementById("carrozzaOut").
+        innerHTML=Math.floor(Math.random() * 10) + 1;
+
+        // ESCLUSO ZERO
+        document.getElementById("prezzointero").innerHTML=`${prezzointero}`;
         document.getElementById("codiceOut").innerHTML=Math.floor(Math.random() * 1000000);
         document.getElementById("costoOut").innerHTML=`${costo}`;
-        console.log(`La tua tariffa scontata del 20% è di ${costo}euro`);
+       
     }
     else{
-        console.log(`La tua tariffa senza sconti è di ${prezzointero}euro`);
+        // mostra row dati ticket
+
         document.getElementById("divOff").style.display = "flex";
+
         document.getElementById("userNameOut").innerHTML=`${userName}`;
         document.getElementById("offertaOut").innerHTML=`${age}`;
-        document.getElementById("carrozzaOut").innerHTML=Math.floor(Math.random() * 11);
+
+        // VALORE DA 1 A 10 ESCLUSO ZERO
+        document.getElementById("carrozzaOut").innerHTML=Math.floor(Math.random() * 10)+1;
+
+        // Escluso Zero
         document.getElementById("codiceOut").innerHTML=Math.floor(Math.random() * 1000000);
-        document.getElementById("costoOut").innerHTML=`${costo}`;
-        console.log(`La tua tariffa scontata del 20% è di ${costo}euro`);
+        document.getElementById("costoOut").innerHTML=`${prezzointero}`;
+        
     }
    
 }
