3b6a286b5f9cffa7b2a125876673f63d4b6cb652
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-08 16:33:35 +0100

    menù a tendina

--- js/main.js
+++ js/main.js
@@ -9,12 +9,12 @@ function(){
     const prezzointero=(numeroKm * prezzo).toFixed(2);
 
 
-    if (age < 17) {
+    if (age == "minore") {
         let sconto= ((numeroKm * prezzo) * 20) / 100 ;
         let costo= (prezzointero - sconto).toFixed(2);
         console.log(`La tua tariffa scontata del 20% è di ${costo}euro`);
     }
-    else if ( age >= 65){
+    else if ( age == "over65"){
         let sconto= (numeroKm * prezzo) * 40 / 100;
         let costo=(prezzointero - sconto).toFixed(2);
         console.log(`La tua tariffa scontata del 40% è di ${costo}euro`);
